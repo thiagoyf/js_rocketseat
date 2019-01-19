1edb77f334e0bc7b0ff70810f8625404f17dd8e8
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-19 10:34:44 -0300

    commiting .editorconfig to change the configuration of the editor, implementing the rendering of todosand changing identations

--- app_todos/todos.js
+++ app_todos/todos.js
@@ -3,8 +3,19 @@ var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
+  'Fazer café',
+  'Estudar Javascript',
+  'Acessar comunidade da Rocketseat'
 ];
 
+function renderTodos() {
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
