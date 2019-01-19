79b99819c50d819703fe25f3f5403001ee27c011
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-01-19 11:22:47 -0300

    commiting adding link to the todoElement and adding a event onClick on the link to remove the element

--- app_todos/todos.js
+++ app_todos/todos.js
@@ -9,11 +9,25 @@ var todos = [
 ];
 
 function renderTodos() {
+  clearListTodos();
+
   for (todo of todos) {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+
+    var position = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + position + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
@@ -30,8 +44,13 @@ function addTodo() {
   todos.push(todoText);
   inputElement.value = '';
 
-  apagarTodosTodos();
   renderTodos();
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(position) {
+  todos.splice(position, 1);
+
+  renderTodos();
+}
